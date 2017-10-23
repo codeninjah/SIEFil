f738c6956720f55c10d5c993f364b16029807e6a
codeninjah <codeninjahblog@gmail.com>
2017-10-23 09:52:56 +0200

    Add project files.

---
+++ SIEFil/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIEFil
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Ange SIE Filens sölväg");
+			var sökväg = Console.ReadLine();
+
+			var lines = File.ReadAllLines(sökväg);
+
+			if (line.Contains("#TRANS")
+				{
+
+				}
+		}
+	}
+}
